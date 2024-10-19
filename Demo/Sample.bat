a79da90570e81b8d42df2d3dcbbf9ad77c271353
Wizou <11647984+wiz0u@users.noreply.github.com>
2024-10-19 15:40:13 +0200

    Mkdir missing empty dirs in Demo

--- Demo/Sample.bat
+++ Demo/Sample.bat
@@ -1,5 +1,9 @@
 @path ..\..\bin
 @del WGenPatDir.pat 2> NUL
+@mkdir dir1\removedDir\removedNoFiles 2> NUL
+@mkdir dir1\removedDirEmpty 2> NUL
+@mkdir dir2\newDir\newDirNoFiles 2> NUL
+@mkdir dir2\newDirEmpty 2> NUL
 WGenPatDir.exe --precise --exclude *.tmp;.svn dir1 dir2
 @echo.
 @echo You can now compile Sample.nsi with NSIS
